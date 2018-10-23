5deb44d34fd66d780ca0c2b18391b1dc37691481
naresh <nareshkumar151993@gmail.com>
2018-10-23 14:30:12 +0530

    test smart changes3

--- src/components/form_signin.js
+++ src/components/form_signin.js
@@ -48,7 +48,7 @@ class FormSignin extends Component {
               <br />
               <Field
                 label="Enter your email"
-                placeholder="Username"
+                placeholder="Email Address or Username"
                 type="text"
                 name="email"
                 component={FormEmail}
