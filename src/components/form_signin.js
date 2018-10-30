036ec4e4a295667c5fe0bb358af7e7a0636fc662
naresh <nareshkumar151993@gmail.com>
2018-10-30 19:08:48 +0530

    smart maintanence - test

--- src/components/form_signin.js
+++ src/components/form_signin.js
@@ -55,7 +55,7 @@ class FormSignin extends Component {
               />
               <Field
                 label="Enter your password"
-                placeholder="pwd"
+                placeholder="pwd25"
                 type="password"
                 name="pword"
                 component={FormPassword}
