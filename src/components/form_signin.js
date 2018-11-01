a2887a3feea9b2974e2e28c9d754d637aceed9e0
Han Song <shak5643@gmail.com>
2018-11-01 10:21:31 -0700

    firstname

--- src/components/form_signin.js
+++ src/components/form_signin.js
@@ -48,7 +48,7 @@ class FormSignin extends Component {
               <br />
               <Field
                 label="Enter your email"
-                placeholder="thatsfine"
+                placeholder="firstname"
                 type="text"
                 name="email"
                 component={FormEmail}
