8543239e49ddc7cdcbe41b1ae8f5bf896d6fcb06
Puneet <puneet@autonomiq.io>
2018-11-06 20:47:13 +0530

    thatscool

--- src/components/form_signin.js
+++ src/components/form_signin.js
@@ -48,7 +48,7 @@ class FormSignin extends Component {
               <br />
               <Field
                 label="Enter your email"
-                placeholder="thatscool"
+                placeholder="okok"
                 type="text"
                 name="email"
                 component={FormEmail}
