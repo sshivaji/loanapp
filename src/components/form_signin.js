ac4bea031bc6bc55b911d7f7199bbf440ee2698d
Puneet <puneet@autonomiq.io>
2018-11-07 15:23:02 +0530

    label change

--- src/components/form_signin.js
+++ src/components/form_signin.js
@@ -48,7 +48,7 @@ class FormSignin extends Component {
               <br />
               <Field
                 label="Enter your email"
-                placeholder="thatsfine"
+                placeholder="finaltest"
                 type="text"
                 name="email"
                 component={FormEmail}
